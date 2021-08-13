8d078b63beb0ae0b25167801432c050b35cdec56
Marcelo Muniz da Luz <ma_celinho18@hotmail.com>
2021-08-13 18:15:18 -0300

    Finished the api

--- FilmesAPI/Data/Dto/CreateFilmeDto.cs
+++ FilmesAPI/Data/Dto/CreateFilmeDto.cs
@@ -4,20 +4,16 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FilmesAPI.Models
+namespace FilmesAPI.Data.Dto
 {
-    public class Filme
+    public class CreateFilmeDto
     {
-        [Key]
-        [Required]
-        public int Id { get; set; }
         [Required(ErrorMessage = "O campo Titulo é obrigatorio.")]
         public string Titulo { get; set; }
-        [Required(ErrorMessage = "O campo Diretor é obrigatorio.")]        
+        [Required(ErrorMessage = "O campo Diretor é obrigatorio.")]
         public string Diretor { get; set; }
         public string Genero { get; set; }
         [Range(1, 600, ErrorMessage = "A duração deve ter no minimo 1 minuto até 600 minutos.")]
         public int Duracao { get; set; }
-
     }
 }
